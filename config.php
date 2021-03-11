cbea16e77707ea606461e8d04f3e9789efff80ac
Hajnik Samuel <samuel.hajnik@bbcag.ch>
2021-03-11 09:34:09 +0100

    added config file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+    // Datebankkonfiguration
+    'database' => array(
+        'host'     => 'localhost',
+        'username' => 'lazzarodb_user',
+        'password' => '12345',
+        'database' => 'lazzarodb',
+    ),
+);
