1e12e7a4feaad2c28ead9e89368d6a90a02df751
zblaar <raphael.blaauw@bbcag.ch>
2021-03-11 15:13:09 +0100

    started main site layout in index.php, added index.css

--- templates/default/index.php
+++ templates/default/index.php
@@ -1,1 +1,146 @@
-Hallo Welt!
+<main class="main">
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <img src="/images/lazzaroLogo.png" class="img-fluid" alt="Logo">
+        </div>
+    </section>
+
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8" id="page-title">
+                    <h4>Reviews</h4>
+                </div>
+                <div class="col-md-3">
+                    <input class="form-control" list="datalistOptions"
+                           placeholder="Type to search...">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago <span class="genre">- House</span></small></p>
+                            <p><small class="text-muted">Comment</small> </p>
+                        </div>
+                        <img src="https://i.pinimg.com/originals/a4/0d/a5/a40da5f4387ea79b17cbfd7b0f5e74f4.png"
+                             class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago <span class="genre">- House</span></small></p>
+                            <p><small class="text-muted">Comment</small> </p>
+                        </div>
+                        <img src="https://i.pinimg.com/originals/a4/0d/a5/a40da5f4387ea79b17cbfd7b0f5e74f4.png"
+                             class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="list-group">
+                        <h5>Genre</h5>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            EDM
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">Pop</a>
+                        <a href="#" class="list-group-item list-group-item-action">Hip Hop</a>
+                        <a href="#" class="list-group-item list-group-item-action">Rock</a>
+
+                        <h5>Jahr</h5>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            2021
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">2020</a>
+                        <a href="#" class="list-group-item list-group-item-action">2002</a>
+                        <a href="#" class="list-group-item list-group-item-action">1981</a>
+                    </div>
+
+                </div>
+
+
+
+
+
+            </div>
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        <img src="..." class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        <img src="..." class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        <img src="..." class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        <img src="..." class="card-img-bottom" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to
+                                additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+</main>
+
+</div>
