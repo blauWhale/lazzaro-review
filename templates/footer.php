c8632fdca95188dea7748be601e267f0e7d65099
Hajnik Samuel <samuel.hajnik@bbcag.ch>
2021-03-11 15:10:45 +0100

    header and footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,24 @@
     </main>
 
-    <footer class="footer">
-      <div class="container">
-        <span class="text-muted">&copy; ICT Berufsbildungcenter AG 2016</span>
-      </div>
-    </footer>
+    <!-- Site footer -->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6" id="footer-content">
+                    <h6> <a href="kontakt.php">Kontakt</a></h6>
+                    <hr>
+                    <h6> <a href="impressum.php">Impressum</a></h6>
+                    <form>
+                        <input class="form-control me-2" id="searchbar" type="search" placeholder="Search" aria-label="Search">
+                    </form>
+                </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                <div class="col-xs-6 col-md-3">
+                    <ul>
+                        <li><a href="index.php"> <img src="/images/lazzaro_logo.png" class="logo-footer" alt="Logo"> </a></li>
+                    </ul>
+                </div>
+        </div>
+    </footer>
   </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -8,27 +8,29 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/style.css" >
 
-    <title><?= $title; ?> | Bbc MVC</title>
+    <title><?= $title; ?> | Home</title>
   </head>
   <body>
 
     <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="/">Bbc MVC</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/user">User</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">Home</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="navbar-brand">
+                            <a class="nav-link active" aria-current="page" href="reviews.php">Reviews</a>
+                        </li>
+                        <li class="navbar-brand">
+                            <a class="nav-link active" href="kontakt.php">Kontakt</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 
     <main class="container">
