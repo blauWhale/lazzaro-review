c7a883ef652facbc5f1aaba5a599965af187aa4c
Hajnik Samuel <samuel.hajnik@bbcag.ch>
2021-03-11 09:21:15 +0100

    adjusted scheme.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,12 +1,11 @@
 ﻿DROP TABLE IF EXISTS user;
 CREATE TABLE  user (
-  id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  firstName VARCHAR(64)  NOT NULL,
-  lastName  VARCHAR(64)  NOT NULL,
+  ID_User        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(64)  NOT NULL,
   email     VARCHAR(128) NOT NULL,
   password  VARCHAR(255)  NOT NULL,
   PRIMARY KEY  (id)
 );
 
-INSERT INTO user (firstName, lastName, email, password) VALUES ('Raphael',  'Blaauw',  'raphael.blaauw@bbcag.ch',   sha2('raphael', 256));
-INSERT INTO user (firstName, lastName, email, password) VALUES ('Samuel', 'Hajnik', 'samuel.hajnik@bbcag.ch', sha2('samuel', 256));
+INSERT INTO user (username, email, password) VALUES ('Raphael',  'raphael.blaauw@bbcag.ch',   sha2('raphael', 256));
+INSERT INTO user (username, email, password) VALUES ('Samuel', 'samuel.hajnik@bbcag.ch', sha2('samuel', 256));
