cb77e64f5fe1889d62fc074aef3c80c211ee2623
Blaauw Raphael <raphael.blaauw@bbcag.ch>
2021-03-12 09:46:44 +0100

    added review-page, including stylesheet, index.php for review and controller

---
+++ templates/review/index.php
@@ -0,0 +1,93 @@
+<main class="main">
+
+    <section class="jumbotron text-center">
+        <div class="container">
+            <img src="/images/lazzaroLogo.png" class="img-fluid" alt="Logo">
+        </div>
+    </section>
+
+    <div class="main-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago <span class="genre">- House</span></small>
+                            </p>
+                            <p><small class="text-muted">Comment</small></p>
+                        </div>
+                        <hr>
+                        <img src="https://i.pinimg.com/originals/a4/0d/a5/a40da5f4387ea79b17cbfd7b0f5e74f4.png"
+                             alt="..." width="250px" height="250px">
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+
+                        <p class"Rating"> Lazzaro Rating: 9/10</p>
+
+                        <label for="customRange3" class="form-label">User Rating</label>
+                        <input type="range" class="form-range" min="0" max="10" step="1" id="customRange3">
+
+                        <a id="sharebutton" class="btn" href="" role="button">Share</a>
+
+                        <p class="tags">Tags: Rock</p>
+
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <input class="form-control" list="datalistOptions"
+                           placeholder="Type to search...">
+
+                    <div class="list-group">
+                        <h5>Genre</h5>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            EDM
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">Pop</a>
+                        <a href="#" class="list-group-item list-group-item-action">Hip Hop</a>
+                        <a href="#" class="list-group-item list-group-item-action">Rock</a>
+
+                        <h5>Jahr</h5>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            2021
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">2020</a>
+                        <a href="#" class="list-group-item list-group-item-action">2002</a>
+                        <a href="#" class="list-group-item list-group-item-action">1981</a>
+                    </div>
+
+                </div>
+            </div>
+            <h2>Kommentare (1)</h2>
+            <div class="row">
+                <div class="col-md-8">
+                    <h5>musikhörer55</h5>
+                    <p class="card-text"><small class="text-muted">12/03/2021 - 09:31</small>
+                    </p>
+                    <p>Lorem Ipsum und so</p>
+
+                    <p><small class="text-muted">Comment</small></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    </div>
+                    <button id="submitbtn" type="submit" class="btn">Kommentieren</button>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+    </div>
+
+</main>
+
+</div>
